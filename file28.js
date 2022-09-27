af28b1fd443019dbcc8a991d88d83292a24a8478
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2022-09-27 08:59:11 +0530

    Add files via upload

---
+++ file28.js
@@ -0,0 +1,12 @@
+//for of loop in array 
+
+const fruits = ["apple" , "mango" , "grapes"];
+
+for(let fruit of fruits){
+    console.log(fruit);    
+}
+
+//for loop
+for(let i = 0 ; i<fruit.length ; i++){
+    console.log(fruit[i]);
+} 
---
+++ file29.js
@@ -0,0 +1,12 @@
+//for in loop
+
+const fruits =["apple" , "mango" , "grapes" , "fruit4"];
+const fruits2 = [];
+
+for(let index in fruits){
+    //console.log(index); ->it gives index 
+    console.log(fruits[index]);
+}
+
+//while and for in loop are not used much
+
