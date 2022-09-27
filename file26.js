9adc1e609cabdddc6a88552db77412a377bf914b
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2022-09-27 08:58:54 +0530

    Add files via upload

---
+++ file26.js
@@ -0,0 +1,13 @@
+//use const for creating array 
+// const pi = 3.14;
+
+// console.log(pi);
+
+//heap memory["apple" , "mango"]; 0x11
+
+const fruits = ["apple" , "mango" ] //0x11
+fruits = ["grapes" , "pineapple"]; //we cannot add elements directly 
+fruits.push("banana");  //but we can use diff array methods like push, pop , concat 
+console.log(fruits);
+
+//but we can easily add by writing let ,but mostly const are used 
---
+++ file27.js
@@ -0,0 +1,10 @@
+//while loop in array 
+
+const fruits = ["apple" , "mango" , "grapes"];
+
+
+let i = 0 ;
+while(i<fruits.length){
+    console.log(fruits[i]);
+    i++;
+}
