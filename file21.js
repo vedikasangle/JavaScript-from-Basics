324ab2246d3cbc012f42c6da23cffb6bad5d06bc
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2022-09-27 08:58:06 +0530

    Add files via upload

---
+++ file21.js
@@ -0,0 +1,23 @@
+//intro to array 
+//reference type
+
+//how to create array
+let fruit = ["apple", "mango" , "grapes"];
+// console.log(fruit[1]);
+
+// let numbers =[1,2,3,4,5];
+// console.log(numbers);
+// let mixed =[1,2.3,4 ,"string" , null ,undefined];
+// console.log(mixed);
+
+let fruits= ["apple" , "mango" , "grapes"];
+let obj = {};  //object literal
+// console.log(fruits);
+// fruits[1] = "banana";
+// console.log(fruits);
+
+console.log(typeof fruits);
+console.log(Array.isArray(fruits)); //to know it is array 
+
+
+//array indexing
---
+++ file22.js
@@ -0,0 +1,33 @@
+///array methods
+//1.array push pop
+
+//array shift unshift
+
+let fruits =["apple" , "banana" , "grapes"];
+console.log(fruits);
+
+// //to add element in last ->push
+// fruits.push("banana");
+// console.log(fruits)
+
+
+// //to remove element from last ->pop
+// let poppedfruit = fruits.pop();
+// console.log("popped fruit is ", poppedfruit);
+// console.log(fruits);
+
+//adding element in starting -> unshift
+// fruits.unshift("banana");
+// fruits.unshift("strawberry")
+// console.log(fruits);
+
+
+//removing element from the starting ->shift
+let removedfruit = fruits.shift();
+console.log("removed fruit is " , removedfruit)
+console.log(fruits);
+
+
+//push and pop are faster than shift and unshift
+
+
