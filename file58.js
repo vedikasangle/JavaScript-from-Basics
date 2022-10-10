2da39e27ac3f2fcb63157e367c1f7356482c21c2
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2022-10-10 23:15:41 +0530

    Add files via upload

---
+++ file58.js
@@ -0,0 +1,14 @@
+//fill method 
+
+//value, start , end 
+
+const myArray = new Array(10).fill(-1);
+console.log(myArray);
+
+
+const myArrayy = [1,2,3,4,5,6,7,8];
+myArrayy.fill(0,2,5)    //(value to be filled, index starting, index ending)
+console.log(myArrayy);
+
+//fill method changes orginal array
+
