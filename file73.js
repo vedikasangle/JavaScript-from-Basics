c3169d75d254b4907d9764f72a6419608d9c16e4
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2022-10-13 22:11:52 +0530

    Add files via upload

--- file73.js
+++ file73.js
@@ -14,15 +14,17 @@ const UserMethods= {
     }
 }
 function createUser(firstName , lastName  , email, age , address){
-    const user = {};
+    const user = Object.create(UserMethods); 
     user.firstName = firstName ;
     user.lastName = lastName ; 
     user.email = email; 
     user.age = age ; 
     user.address = address ; 
-    user.about = UserMethods.about; //reference is stored
-    user.is18 = UserMethods.is18; 
-    user.sing = UserMethods.sing; 
+    // user.about = UserMethods.about; //reference is stored
+    // user.is18 = UserMethods.is18; 
+    // user.sing = UserMethods.sing; 
+
+
 
     return user;
 }
@@ -32,6 +34,6 @@ function createUser(firstName , lastName  , email, age , address){
 const user1 = createUser('harshit', 'idk' , 'vedika@gmail.com' , 3 , 'myaddressq' );
 const user2 = createUser('vedika', 'ik' , 'vediiika@gmail.com' , 91 , 'myaddressww' );
 const user3 = createUser('harsh', 'idkk' , 'veeedika@gmail.com' , 30 , 'myaddressa' );
-
-console.log(user1.sing());
-console.log(user3.about());
+console.log(user1);
+// console.log(user1.sing());
+ console.log(user3.about());
