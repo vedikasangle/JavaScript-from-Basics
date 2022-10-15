19c5e31fd9219bf43d5373daf356748885382530
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2022-10-15 23:35:18 +0530

    Add files via upload

---
+++ file87.js
@@ -0,0 +1,11 @@
+//function expression 
+
+console.log(myfunction);
+var myfunction = function(){
+    console.log("this is my function ");
+}
+  
+
+console.log(myfunction);
+
+
---
+++ file88.js
@@ -0,0 +1,8 @@
+//console.log(firstName);  -->temporal dead zone(tdz)
+let firstName = "vedika"; 
+console.log(firstName);
+
+
+console.log(typeof firstNamee)
+// let firstNamee = "sangle";
+
