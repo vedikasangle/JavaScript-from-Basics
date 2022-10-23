ca9ac3643118ca9f5ca99ff68f8452c41887a6a5
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2022-10-23 22:27:50 +0530

    Add files via upload

---
+++ file97.js
@@ -0,0 +1,8 @@
+//select element using query selector
+//can be used for id or classes 
+
+const mainHeading  = document.querySelector("#main-heading");
+const header = document.querySelector(".header")
+console.log(mainHeading)
+
+
