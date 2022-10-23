8aa22c1a00112b3bfdf75df2ecb23c9520d44ddf
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2022-10-23 22:27:36 +0530

    Add files via upload

---
+++ file96.js
@@ -0,0 +1,15 @@
+//DOM
+// document object model
+
+//overview 
+// console.dir(document) 
+
+//there are different methods to select elements 
+
+//select element using get element by id 
+
+console.dir(document.getElementById("main-heading"))  //this returns an object 
+console.log(typeof document.getElementById("main-heading"))
+const mainHeading  = document.getElementById("main-heading");
+
+console.log(mainHeading)
