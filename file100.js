cd392df08f7ae3f1f7b7cec033aa271320da174c
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2022-10-24 08:13:05 +0530

    Add files via upload

---
+++ file100.js
@@ -0,0 +1,8 @@
+//change the style of elements 
+
+const mainHeading = document.querySelector("div.headline h2")
+// console.log(mainHeading.style)
+mainHeading.style.color = "blue";
+// background-color ->not allowed
+mainHeading.style.backgroundColor = "yellow";
+mainHeading.style.border = "10px solid green";
---
+++ file99.js
@@ -0,0 +1,9 @@
+//change text
+//textcontent and innertext
+ const mainHeading = document.getElementById("main-heading");
+ console.log(mainHeading.textContent);
+//  mainHeading.textContent="this is something else";
+//  console.log(mainHeading.textContent);
+
+
+//innertext also property in js  
